a8cc2b561475a9496a9f36e7f2acd989971cbbf4
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-24 16:58:25 -0500

    server sends new maps to client every 100ms

--- client/index.js
+++ client/index.js
@@ -1,34 +1,28 @@
 /* eslint-disable no-shadow */
 import { direction } from './keypress';
+const socket = io();
 
 // constants
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 600;
 const CELL_SIZE = 10;
-const BOARD_WIDTH = 80;
-const BOARD_HEIGHT = 60;
 
 // initialize canvas
 const canvas = document.getElementById('canvas');
-canvas.width = CELL_SIZE * BOARD_WIDTH;
-canvas.height = CELL_SIZE * BOARD_HEIGHT;
-canvas.style.border = '2px solid black';
+canvas.width = 800;
+canvas.height = 600;
 const context = canvas.getContext('2d');
-
-// create an empty map
-const mapFactory = () => {
-  return Array.from({ length: BOARD_HEIGHT }, () =>
-    Array.from({ length: BOARD_WIDTH }, () => Math.floor(Math.random() * 2))
-  );
-};
-const map = mapFactory();
 
 // draw the map
 const renderMap = (context, map) => {
-  for (let y = 0; y < BOARD_HEIGHT; y += 1) {
-    for (let x = 0; x < BOARD_WIDTH; x += 1) {
+  for (let y = 0; y < map.length; y += 1) {
+    for (let x = 0; x < map[0].length; x += 1) {
       const currentCell = map[y][x];
       context.fillStyle = currentCell ? '#000000' : '#ffffff';
       context.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
     }
   }
 };
-renderMap(context, map);
+
+// render inside socket
+socket.on('state', state => renderMap(context, state.map));
--- server/index.js
+++ server/index.js
@@ -37,6 +37,25 @@ io.on('connection', socket => {
  *    GAME
  **************/
 
+// constants
+const BOARD_WIDTH = 80;
+const BOARD_HEIGHT = 60;
+const UPDATES_PER_SECOND = 10;
+
+// create an empty map
+const mapFactory = () => {
+  return Array.from({ length: BOARD_HEIGHT }, () =>
+    Array.from({ length: BOARD_WIDTH }, () => Math.floor(Math.random() * 10))
+  );
+};
+
+setInterval(() => {
+  const map = mapFactory();
+  io.emit('state', { map });
+}, 100);
+
+// listening
 io.on('connection', socket => {
+  // listen for direction changes
   socket.on('direction', direction => console.log(direction));
 });
