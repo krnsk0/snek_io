e6c1ea258c7d40b677197d9371ae74a378c5ee5e
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-25 15:38:31 -0500

    add name form

---
+++ client/game.js
@@ -0,0 +1,53 @@
+/* global io */
+import { setUpKeyListeners } from './keypress';
+
+// constants
+const BOARD_WIDTH = 80;
+const BOARD_HEIGHT = 60;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 600;
+const CELL_SIZE = 10;
+
+// initialize canvas
+const canvas = document.getElementById('canvas');
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = '#222222';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// render
+const renderMap = (ctx, map) => {
+  for (let y = 0; y < map.length; y += 1) {
+    for (let x = 0; x < map[0].length; x += 1) {
+      const currentCell = map[y][x];
+      ctx.fillStyle = currentCell ? currentCell : `#000000`;
+      // the +1 allows the bg to come through as grid lines
+      ctx.fillRect(
+        x * CELL_SIZE + 1,
+        y * CELL_SIZE + 1,
+        CELL_SIZE - 1,
+        CELL_SIZE - 1
+      );
+    }
+  }
+};
+
+const startGame = name => {
+  const socket = io();
+  setUpKeyListeners(socket);
+
+  // save this connection's id
+  let id = '';
+  socket.on('connect', () => {
+    id = socket.id;
+    console.log('connection id:', id);
+  });
+
+  // receive and paint initial map sent by server
+  socket.on('sync_map', map => {
+    renderMap(ctx, map);
+  });
+};
+
+export default startGame;
--- client/index.js
+++ client/index.js
@@ -1,52 +1,14 @@
 /* eslint-disable no-shadow */
-/* global io */
-import { setUpKeyListeners } from './keypress';
+import startGame from './game';
 
-// constants
-const BOARD_WIDTH = 80;
-const BOARD_HEIGHT = 60;
-const CANVAS_WIDTH = 800;
-const CANVAS_HEIGHT = 600;
-const CELL_SIZE = 10;
-
-// set up socket
-const socket = io();
-
-// initialize keypress module
-setUpKeyListeners(socket);
-
-// initialize canvas
-const canvas = document.getElementById('canvas');
-canvas.width = CANVAS_WIDTH;
-canvas.height = CANVAS_HEIGHT;
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = '#222222';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-const renderMap = (ctx, map) => {
-  for (let y = 0; y < map.length; y += 1) {
-    for (let x = 0; x < map[0].length; x += 1) {
-      const currentCell = map[y][x];
-      ctx.fillStyle = currentCell ? currentCell : `#000000`;
-      // the +1 allows the bg to come through as grid lines
-      ctx.fillRect(
-        x * CELL_SIZE + 1,
-        y * CELL_SIZE + 1,
-        CELL_SIZE - 1,
-        CELL_SIZE - 1
-      );
-    }
+// popup
+const popupElement = document.getElementById('popup');
+const formElement = document.getElementById('name-form');
+formElement.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const name = evt.target.name.value;
+  if (name) {
+    popupElement.style.display = 'none';
+    startGame(name);
   }
-};
-
-// save this connection's id
-let id = '';
-socket.on('connect', () => {
-  id = socket.id;
-  console.log('connection id:', id);
-});
-
-// receive and paint initial map sent by server
-socket.on('sync_map', map => {
-  renderMap(ctx, map);
 });
