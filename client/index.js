8660fbbde5ad7a404fbb0b78e3f07c785c5ecb26
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-27 13:22:50 -0500

    fixes leaderboard appearing before game start

--- client/index.js
+++ client/index.js
@@ -5,12 +5,14 @@ import startGame from './game';
 const popupElement = document.getElementById('popup');
 const formElement = document.getElementById('name-form');
 const inputElement = document.getElementById('name-field');
+const leaderBoardDiv = document.getElementById('leaderboard-container');
 inputElement.focus();
 formElement.addEventListener('submit', evt => {
   evt.preventDefault();
   const name = evt.target.name.value;
   if (name) {
     popupElement.style.display = 'none';
+    leaderBoardDiv.style.display = 'block';
     startGame(name);
   }
 });
