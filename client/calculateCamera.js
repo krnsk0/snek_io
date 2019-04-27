83a2d5c74b3d9fad0d926a53cde168998b808427
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-27 10:25:12 -0500

    add zoom constant

--- client/calculateCamera.js
+++ client/calculateCamera.js
@@ -3,18 +3,32 @@ const constants = require('../shared/constants');
 let CELL_SIZE;
 let CAMERA_WIDTH;
 let CAMERA_HEIGHT;
+let BASELINE_HEIGHT_CELLS = 45;
+let BASELINE_WIDTH_CELLS = Math.ceil(BASELINE_HEIGHT_CELLS * constants.ZOOM);
 
+// changes stuff on window resize event
 window.addEventListener('resize', () => {
   CELL_SIZE = Math.floor(
-    Math.min(window.innerHeight / 45, window.innerWidth / 80)
+    Math.max(
+      window.innerHeight / BASELINE_HEIGHT_CELLS,
+      window.innerWidth / BASELINE_WIDTH_CELLS
+    )
   );
 
-  CAMERA_WIDTH = Math.min(Math.floor(window.innerWidth / CELL_SIZE), 80);
-  CAMERA_HEIGHT = Math.min(Math.floor(window.innerHeight / CELL_SIZE), 45);
+  CAMERA_WIDTH = Math.min(
+    Math.floor(window.innerWidth / CELL_SIZE),
+    BASELINE_WIDTH_CELLS
+  );
+  CAMERA_HEIGHT = Math.min(
+    Math.floor(window.innerHeight / CELL_SIZE),
+    BASELINE_HEIGHT_CELLS
+  );
 });
 
+// initialize on page load
 window.dispatchEvent(new Event('resize'));
 
+// closures to allow access to these vars
 export const calculateCameraWidth = () => {
   return CAMERA_WIDTH;
 };
--- shared/constants.js
+++ shared/constants.js
@@ -2,6 +2,7 @@ module.exports = Object.freeze({
   MAP_WIDTH: 150,
   MAP_HEIGHT: 150,
   SERVER_TICKS_PER_SECOND: 20,
+  ZOOM: 1.8, // float between 0.5 and 1.8
   MSG: {
     CONNECTION: 'connection',
     DISCONNECT: 'disconnect',
