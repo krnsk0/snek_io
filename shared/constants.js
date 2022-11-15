3b9b7666909c134866cd4c6d48aecd6b811809b6
Jon Kurinsky <jkurinsky@fastmail.com>
2022-11-15 13:35:42 -0500

    larger map

--- shared/constants.js
+++ shared/constants.js
@@ -1,8 +1,8 @@
 module.exports = Object.freeze({
-  MAP_WIDTH: 150, // no smaller than 150
-  MAP_HEIGHT: 150, // no smaller than 120
+  MAP_WIDTH: 400, // no smaller than 150
+  MAP_HEIGHT: 400, // no smaller than 120
   SERVER_TICKS_PER_SECOND: 15,
-  ZOOM: 1.8, // float between 0.5 and 1.8
+  ZOOM: 1.6, // float between 0.5 and 1.8
   SERVER_LOG_TIME_SECS: 5,
   MAX_FOOD: 250,
   INITIAL_SNAKE_LENGTH: 10,
@@ -15,7 +15,7 @@ module.exports = Object.freeze({
     SET_NAME: 'N',
     DIRECTION: 'D',
     SEND_INITIAL_STATE: 'I',
-    GET_NAME: 'G'
+    GET_NAME: 'G',
   },
   KEYS: {
     PLAYERS: 'P',
@@ -23,6 +23,6 @@ module.exports = Object.freeze({
     LEAVE: 'L',
     FOOD: 'F',
     MAKE: 'M',
-    EAT: 'E'
-  }
+    EAT: 'E',
+  },
 });
